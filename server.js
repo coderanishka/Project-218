092d56587ae2013a98cb8621c62e99699ca773fd
Anishka <anishkagupta167@gmail.com>
2022-09-20 16:12:09 +0530

    project 219

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json())
+
+const { v4: uuidv4 } = require("uuid");
 
 const io = require("socket.io")(server, {
     cors: {
@@ -10,14 +13,26 @@ const io = require("socket.io")(server, {
     }
 });
 
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+
+app.use("/peerjs", peerServer);
+
 app.get("/", (req, res) => {
-    res.render("index");
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
 });
 
 io.on("connection", (socket) => {
-    socket.on("message", (message, user) => {
-        io.emit("createMessage", message, user);
-    });
+    socket.on("join-room", (roomId, userId) => {
+        socket.join(roomId);
+        io.to(roomId).emit("user-connected", userId);
+    })
 });
 
-server.listen(3030);
+server.listen(process.env.PORT || 3030);
